ba1ed427d90199cc2721b622e31091637e9f2ecd
Varan Gill <gillzx01@gmail.com>
2023-11-27 19:53:48 -0500

    Setup imports for pgsql and environmental variables

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,20 +1,15 @@
 import express from "express";
+import { Client } from "pg";
+import dotenv from "dotenv";
+import path from "path";
+
+dotenv.config({ path: path.join(__dirname, "..", ".env") });
 const app = express();
 const port = 3000;
 
-app.use(express.static(__dirname+'/view'))
-
-app.get('/', (req, res) => {
-  res.render('/index');
-});
-app.get('/MyClubs', (req, res) => {
-  res.render('/view/MyClubs');
-});
-app.get('/AllClubs', (req, res) => {
-  res.render('/views/AllClubs');
-});
-app.get('/profile', (req, res) => {
-  res.render('/views/profile');
+app.get("/", (req, res) => {
+  console.log(process.env.DATABASE);
+  res.send("Hello World!");
 });
 
 app.listen(port, () => {
