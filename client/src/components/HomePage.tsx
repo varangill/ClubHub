b89a83957a7687fd98946511dd46f520ea482134
kravich2 <kravich2@uwo.ca>
2023-12-04 22:06:36 -0500

    Finished Home Page v1
    finished up the home page

--- client/src/components/HomePage.tsx
+++ client/src/components/HomePage.tsx
@@ -1,9 +1,6 @@
 import React from "react";
 import NavBar from "./NavigationBar";
-import welcomeImage from "keerthenravichandran/Downloads/ClubHub/server/UWO.jpeg";
-
- 
-
+import welcomeImage from "/Users/keerthenravichandran/Downloads/ClubHub/UWO.jpeg";
 
 export default function HomePage() {
   const welcomeToStyle = {
@@ -14,15 +11,36 @@ export default function HomePage() {
   return (
     <div style={{ backgroundColor: "#c080d0", color: "white", height: "100vh", textAlign: "center", position: "relative" }}>
       <NavBar />
-      <div id="title-content" style={{ margin: "1em 0", position: "absolute", top: "45%", left: "20%", transform: "translate(-50%, -50%)", maxWidth: "60%" }}>
-        <p style={welcomeToStyle}>Welcome to:</p>
-        <p style={{ fontSize: "8em", fontWeight: "bold" }}>ClubHub</p>
+
+      {/* Welcome to Section */}
+      <div id="welcome-section" style={{ position: "absolute", top: "40%", left: "30%", transform: "translate(-50%, -50%)", maxWidth: "80%" }}>
+        <div id="title-content" style={{ margin: "1em 0", textAlign: "left" }}>
+          <p style={welcomeToStyle}>Welcome to:</p>
+        </div>
       </div>
-      <div id="paragraph-content" style={{ fontFamily: "Roboto, sans-serif", margin: "1em 0", position: "absolute", top: "70%", right: "31%", transform: "translate(50%, -50%)", maxWidth: "40%", fontSize: "1.2em" }}>
-        <p>
-          ClubHub is Western University's official gateway to the dynamic world of campus clubs. ClubHub provides students with a seamless experience to explore, connect, and immerse themselves in the diverse tapestry of our extracurricular scene, 
-          fostering a vibrant university community.
-        </p>
+
+      {/* ClubHub Section */}
+      <div id="clubhub-section" style={{ position: "absolute", top: "50%", left: "27%", transform: "translate(-50%, -50%)", maxWidth: "80%" }}>
+        <div id="title-content" style={{ margin: "1em 0", textAlign: "left" }}>
+          <p style={{ fontSize: "8em", fontWeight: "bold", marginLeft: "1em" }}>ClubHub</p>
+        </div>
+      </div>
+
+      {/* Photo Section */}
+      <div id="photo-section" style={{ position: "absolute", top: "40%", left: "70%", transform: "translate(-50%, -50%)" }}>
+        <div id="image-content" style={{ marginLeft: "1em" }}>
+          <img src={welcomeImage} alt="Welcome Image" style={{ width: "120%", height: "auto" }} />
+        </div>
+      </div>
+
+      {/* Paragraph Section */}
+      <div id="paragraph-section" style={{ position: "absolute", top: "70%", left: "75%", transform: "translate(-50%, -50%)", maxWidth: "80%" }}>
+        <div id="paragraph-content" style={{ fontFamily: "Roboto, sans-serif", fontSize: "1.2em" }}>
+          <p>
+            ClubHub is Western University's official gateway to the dynamic world of campus clubs. ClubHub provides students with a seamless experience to explore, connect, and immerse themselves in the diverse tapestry of our extracurricular scene, 
+            fostering a vibrant university community.
+          </p>
+        </div>
       </div>
     </div>
   );
