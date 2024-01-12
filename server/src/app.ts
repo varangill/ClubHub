fa5df45afcaac58eb711f8e69497845c0802fa59
Varan Gill <gillzx01@gmail.com>
2024-01-12 14:19:28 -0500

    Setup Swagger for API documentation

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,6 +3,8 @@ import express from "express";
 import dotenv from "dotenv";
 import path from "path";
 import cors from "cors";
+import swaggerJsdoc from "swagger-jsdoc";
+import swaggerUi from "swagger-ui-express";
 
 import routes from "./routes";
 
@@ -14,6 +16,19 @@ app.use(cors());
 const serverPort = 3000;
 
 app.use("/api", routes);
+
+const swaggerOptions = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "ClubHub API Docs",
+      version: "1.0.0",
+    },
+  },
+  apis: ["./src/routes/*.ts"], // Point to your API route files
+};
+const specs = swaggerJsdoc(swaggerOptions);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 
 //Serve static front-end
 const clientBuild = path.join(__dirname, "../../client/dist");
