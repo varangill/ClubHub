f7ba5f8a915fc45d3f689b60c071a037c1f0a1d9
Varan Gill <gillzx01@gmail.com>
2024-01-12 20:46:01 -0500

    Add API queries to club listing pages

--- client/src/components/MyClubs.tsx
+++ client/src/components/MyClubs.tsx
@@ -1,18 +1,25 @@
 import ClubList from "./ClubList";
 import NavBar from "./NavigationBar";
+import { useEffect, useState } from "react";
+import { getData } from "../api";
+import { useAuth } from "../AuthContext";
 
 export default function MyClubs() {
-  const initialClubs = [
-    { name: "Tennis Club", id: "tennis" },
-    { name: "Chess Club", id: "chess" },
-    { name: "Reading Club", id: "reading" },
-    { name: "Science Club", id: "science" },
-  ];
+  const [clubs, setClubs] = useState([]);
+  const { user } = useAuth();
+
+  useEffect(() => {
+    getData(`users/clubs/${user?.id}`).then((res) => {
+      setClubs(res);
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div className="my-clubs-container">
       <NavBar />
       <h2 className="club-heading">My Clubs</h2>
-      <ClubList clubs={initialClubs} />
+      <ClubList clubs={clubs} />
     </div>
   );
 }
