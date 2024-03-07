2835dfb87e0f2334721d9f30ce4b244ddce4e043
Jchuang7 <jchuang7@uwo.ca>
2024-03-07 16:01:43 -0500

    Updated Swagger Docs
    Updated Swagger Docs with the announcement routes

--- server/src/routes/announcementRoutes.ts
+++ server/src/routes/announcementRoutes.ts
@@ -1,4 +1,72 @@
-
+/**
+ * @swagger
+ * tags:
+ *   name: Announcements
+ *   description: Announcement Management
+ * /announcements/{id}:
+ *   get:
+ *     summary: Gets announcement based on id
+ *     description: Returns announcement based on id
+ *     tags: [Announcements]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Announcement ID
+ *     responses:
+ *       '200':
+ *         description: Successful response         
+ *   delete:
+ *     summary: Deletes announcement based on ID
+ *     description: Delete announcement based on ID
+ *     tags: [Announcements]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Announcement ID
+ *     responses:
+ *       '201':
+ *         description: Announcement Deleted
+ * /announcements/club/{id}:
+ *   get:
+ *     summary: Gets announcements based on ClubId
+ *     description: Returns announcement based on ClubId
+ *     tags: [Announcements]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: Club ID
+ *     responses:
+ *       '200':
+ *         description: Successful response
+ * /announcements/create-announcement:
+ *   post:
+ *     summary: Create a new announcement
+ *     description: Create a new announcement with the provided data
+ *     tags: [Announcements]
+ *     requestBody:
+ *       description: Announcement data
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               clubId:
+ *                 type: string
+ *                 format: id
+ *                 description: Club Id
+ *               userId:
+ *                 type: string
+ */
 
 import { Router } from "express";
 import announcementController from "../controllers/announcementController";
