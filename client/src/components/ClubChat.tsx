91011e34ed56aafdf761ea8f0b1e83a22b577839
Varan Gill <gillzx01@gmail.com>
2024-03-13 15:08:48 -0400

    Make chatbox scrollable and default to bottom of chat

--- client/src/components/ClubChat.tsx
+++ client/src/components/ClubChat.tsx
@@ -1,16 +1,19 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import io from "socket.io-client";
 import { postData, getData } from "../api";
+import Button from "react-bootstrap/Button";
 
 const socket = io("http://localhost:3000");
 
 export default function ClubChat(props) {
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState([]);
+  const messagesBottomRef = useRef(null); //keeps track of bottom of chatbox
 
   useEffect(() => {
     getData(`messages/club/${props.clubId}`).then((res) => {
       setMessages(res);
+      scrollToBottom();
     });
 
     socket.emit("loadClub", props.clubId);
@@ -26,6 +29,10 @@ export default function ClubChat(props) {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const scrollToBottom = () => {
+    messagesBottomRef.current?.scrollIntoView();
+  };
+
   const sendMessage = () => {
     postData(`messages/send`, {
       userId: props.userId,
@@ -34,19 +41,34 @@ export default function ClubChat(props) {
       username: props.userName,
     });
     setMessage("");
+    scrollToBottom();
   };
 
   return (
     <div>
-      <input
-        type="text"
-        value={message}
-        onChange={(e) => setMessage(e.target.value)}
-      />
-      <button onClick={sendMessage}>Send</button>
-      {messages.map((msg, index) => (
-        <p key={index}>{msg["text"]}</p>
-      ))}
+      <center>
+        <h2>Club Messaging Board</h2>
+      </center>
+      <div className="chat-messages-container">
+        <div className="chat-messages">
+          {messages.map((msg, index) => (
+            <p key={index}>
+              {msg["username"]}: {msg["text"]}
+            </p>
+          ))}
+          <div ref={messagesBottomRef} />{" "}
+          {/* empty div to track bottom of chat */}
+        </div>
+        <input
+          type="text"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          className="chat-text-input"
+        />
+        <Button onClick={sendMessage} className="chat-send-button">
+          Send
+        </Button>
+      </div>
     </div>
   );
 }
--- client/src/components/ClubPage.tsx
+++ client/src/components/ClubPage.tsx
@@ -165,110 +165,114 @@ export default function ClubPage() {
     );
   };
   return (
-    <div className="club-detail-container">
-      <NavBar />
+    <div className="club-page-container">
+      <div className="club-detail-container">
+        <NavBar />
 
-      {showMemberModal && (
-        <MemberEditModal
-          hideModal={() => {
-            setShowMemberModal(false);
-          }}
-          member={selectedMember}
-          memberType={memberType}
-          userId={user?.id}
-          clubId={id}
-          requestUpdate={updateMemberList}
-        />
-      )}
-      {showSettingsModal && (
-        <ClubSettingsModal
-          hideModal={() => {
-            setShowSettingsModal(false);
-          }}
-          requestUpdate={updateClubInfo}
-          clubId={id}
-          originalName={clubName}
-          originalDesc={clubDesc}
-          originalStatus={clubStatus}
-          isOwner={memberType === "owner"}
-        />
-      )}
-      {showCreateAnnouncementModal && (
-        <AnnouncementCreationModal
-          hideModal={() => {
-            setShowCreateAnnouncementModal(false);
-          }}
-          clubId={id}
-          userId={user?.id}
-          requestUpdate={updateAnnouncementList}
-        />
-      )}
-      {showAnnouncementModal && (
-        <AnnouncementsModal
-          hideModal={() => {
-            setShowAnnouncementModal(false);
-          }}
-          clubId={id}
-          announcement={selectedAnnouncement}
-        />
-      )}
-      <h2 className="club-heading">{clubName}</h2>
-      <h5 class="club-desc">{clubDesc}</h5>
+        {showMemberModal && (
+          <MemberEditModal
+            hideModal={() => {
+              setShowMemberModal(false);
+            }}
+            member={selectedMember}
+            memberType={memberType}
+            userId={user?.id}
+            clubId={id}
+            requestUpdate={updateMemberList}
+          />
+        )}
+        {showSettingsModal && (
+          <ClubSettingsModal
+            hideModal={() => {
+              setShowSettingsModal(false);
+            }}
+            requestUpdate={updateClubInfo}
+            clubId={id}
+            originalName={clubName}
+            originalDesc={clubDesc}
+            originalStatus={clubStatus}
+            isOwner={memberType === "owner"}
+          />
+        )}
+        {showCreateAnnouncementModal && (
+          <AnnouncementCreationModal
+            hideModal={() => {
+              setShowCreateAnnouncementModal(false);
+            }}
+            clubId={id}
+            userId={user?.id}
+            requestUpdate={updateAnnouncementList}
+          />
+        )}
+        {showAnnouncementModal && (
+          <AnnouncementsModal
+            hideModal={() => {
+              setShowAnnouncementModal(false);
+            }}
+            clubId={id}
+            announcement={selectedAnnouncement}
+          />
+        )}
+        <h2 className="club-heading">{clubName}</h2>
+        <h5 class="club-desc">{clubDesc}</h5>
 
-      {/* Render join button if user isn't a member, otherwise render the leave button for non-owners (members, executives) */}
-      {memberType != "member" && <SettingsButton />}
-      {memberType === "none" && clubStatus === "open" ? (
-        <JoinButton />
-      ) : memberType === "none" && clubStatus === "application" ? (
-        <ApplyButton />
-      ) : memberType === "owner" ? null : (
-        <LeaveButton />
-      )}
-      <div class="first-row">
-        <div class="members">
-          <h1>Members</h1>
-          {members.map((member) => {
-            return (
-              <div key={member["userId"]}>
-                {member["name"]}
-                {user?.id != member["userId"] ? ( //Don't render menu option if member is the logged in user
-                  <MoreVertical
-                    onClick={() => {
-                      setSelectedMember(member);
-                      setShowMemberModal(true);
-                    }}
-                  />
-                ) : null}
-              </div>
-            );
-          })}
-        </div>
-        <div class="announcement-container">
-          <div class="announcement-header">
-            <h1>Announcements</h1>
-          </div>
-          <div class="scroll">
-            {announcements.map((announcement, index) => {
+        {/* Render join button if user isn't a member, otherwise render the leave button for non-owners (members, executives) */}
+        {memberType != "member" && <SettingsButton />}
+        {memberType === "none" && clubStatus === "open" ? (
+          <JoinButton />
+        ) : memberType === "none" && clubStatus === "application" ? (
+          <ApplyButton />
+        ) : memberType === "owner" ? null : (
+          <LeaveButton />
+        )}
+        <div class="first-row">
+          <div class="members">
+            <h1>Members</h1>
+            {members.map((member) => {
               return (
-                <ul
-                  key={announcement["id"]}
-                  class="announcement"
-                  onClick={() => {
-                    setSelectedAnnouncement(announcement);
-                    setShowAnnouncementModal(true);
-                  }}
-                >
-                  {announcement["announcementTitle"]} -{" "}
-                  {announcement["announcementText"]}
-                </ul>
+                <div key={member["userId"]}>
+                  {member["name"]}
+                  {user?.id != member["userId"] ? ( //Don't render menu option if member is the logged in user
+                    <MoreVertical
+                      onClick={() => {
+                        setSelectedMember(member);
+                        setShowMemberModal(true);
+                      }}
+                    />
+                  ) : null}
+                </div>
               );
             })}
           </div>
-          {memberType != "member" && <AnnouncementCreationButton />}
+          <div class="announcement-container">
+            <div class="announcement-header">
+              <h1>Announcements</h1>
+            </div>
+            <div class="scroll">
+              {announcements.map((announcement, index) => {
+                return (
+                  <ul
+                    key={announcement["id"]}
+                    class="announcement"
+                    onClick={() => {
+                      setSelectedAnnouncement(announcement);
+                      setShowAnnouncementModal(true);
+                    }}
+                  >
+                    {announcement["announcementTitle"]} -{" "}
+                    {announcement["announcementText"]}
+                  </ul>
+                );
+              })}
+            </div>
+            {memberType != "member" && <AnnouncementCreationButton />}
+          </div>
         </div>
+        {showPopup && <div className="popup">Club has been joined!</div>}
       </div>
-      <ClubChat clubId={id} userId={user?.id} userName={user?.name} />
-      {showPopup && <div className="popup">Club has been joined!</div>}
+      <div>
+        <ClubChat clubId={id} userId={user?.id} userName={user?.name} />
+      </div>
     </div>
   );
 }
