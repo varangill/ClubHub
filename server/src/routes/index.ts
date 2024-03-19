dfc956b2f97b6077c8f65f87fdcb8cd84c5ca465
Varan Gill <gillzx01@gmail.com>
2024-03-19 00:51:47 -0400

    Add tags router to main router

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -6,11 +6,13 @@ import clubRoutes from "./clubRoutes";
 import announcementRoutes from "./announcementRoutes";
 import applicationRoutes from "./applicationRoutes";
 import messageRoutes from "./messageRoutes";
+import tagRoutes from "./tagRoutes";
 
 router.use("/users", userRoutes);
 router.use("/clubs", clubRoutes);
 router.use("/announcements", announcementRoutes);
 router.use("/applications", applicationRoutes);
 router.use("/messages", messageRoutes);
+router.use("/tags", tagRoutes);
 
 export default router;
